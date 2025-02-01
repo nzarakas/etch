65196898267d1869c333c22f8d09906b4293c5b8
Nicolas Zarakas <nzarakas1@gmail.com>
2025-02-01 21:32:04 +0200

    Added dynamic scaling of container according to gridblock size. Resized body & container. Added prompt value filters.

--- etch.js
+++ etch.js
@@ -1,32 +1,111 @@
+const body = document.querySelector("body");
+
+const btn = document.createElement("button");
+btn.textContent = "press me!"
+
+
 
 const container = document.querySelector("#container");
-for(let i = 0; i<48*48;i++){
-    const gridBlock = document.createElement("div");
-    // gridBlock.textContent = `${i}`;
-    gridBlock.classList.add("grid");
-    container.appendChild(gridBlock);
+// let rows = 0 ;
+// let columns = 0 ; 
+
+
+btn.addEventListener("click", ()=>{
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    let rows = 0;
+    let columns = 0;
     
-}
 
-const blocks = document.querySelectorAll(".grid");
+    //make sure propmpt number is small enough that it won't crash app
+    do{
+        rows =  +prompt("How many rows in new etch a sketch board?", "40");  
+    }while(Number.isNaN(rows) || rows>100);
 
-let isMouseDown = false;
+    //calculating how many columns grid should have and 
+    // resizes grid dynamically based on # of columns
+    columns = Math.floor(rows*1.33);
+    gridBlockWidth = Math.floor(1020/columns);
+    gridBlockHeight = Math.floor(760/rows);
+    container.style.width = gridBlockWidth*columns + "px";
+    container.style.height = gridBlockHeight*rows + "px";
 
-document.addEventListener("mousedown", (e)=>{
+    for(let i = 0; i<rows*columns;i++){
+        const gridBlock = document.createElement("div");
+        gridBlock.id = `${i+1}`;
+        gridBlock.classList.add("grid");
+        gridBlock.style.width = gridBlockWidth + "px";
+        gridBlock.style.height = gridBlockHeight + "px";
+        container.appendChild(gridBlock);  
+    }
+    
+
+    // This part is about clicking on a grid tile and changing its colour
+    // effectively playing the etch a sketch game
+    const blocks = document.querySelectorAll(".grid");
+    
+
+    let isMouseDown = false;
+
+    document.addEventListener("mousedown", (e)=>{
         if(e.button === 0){
             isMouseDown = true;
         }
     });
-document.addEventListener("mouseup",() =>{
+    document.addEventListener("mouseup",() =>{
         isMouseDown = false;
     });
 
 
-blocks.forEach(block => {
-    block.addEventListener("mouseover", () =>{
-        if (isMouseDown) {
-            block.style.backgroundColor = 'black';   
+    blocks.forEach(block => {
+        //block.style.backgroundColor = 'white';
+        block.addEventListener("mouseover", () =>{
+            if (isMouseDown) {
+                block.style.backgroundColor = 'black';
             }
         }
-   )
-})
+    )
+
+    })
+
+});
+
+body.insertBefore(btn,container);
+
+
+// btn.addEventListener("click", ()=>{
+//     container.removeChild(gridBlock);
+// })
+// console.log(rows);
+// for(let i = 0; i<rows*rows;i++){
+//     const gridBlock = document.createElement("div");
+//     gridBlock.classList.add("grid");
+//     container.appendChild(gridBlock);  
+// }
+
+// body.insertBefore(btn,container);
+
+
+// const blocks = document.querySelectorAll(".grid");
+
+// let isMouseDown = false;
+
+// document.addEventListener("mousedown", (e)=>{
+//         if(e.button === 0){
+//             isMouseDown = true;
+//         }
+//     });
+// document.addEventListener("mouseup",() =>{
+//         isMouseDown = false;
+//     });
+
+
+// blocks.forEach(block => {
+//     block.addEventListener("mouseover", () =>{
+//         if (isMouseDown) {
+//             block.style.backgroundColor = 'black';   
+//             }
+//         }
+//    )
+// })
